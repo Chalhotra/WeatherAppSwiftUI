ea38ced6552d6d90dde20fa53e32a472c86ab5bb
Soumil Malhotra <malhotrasoumil@gmail.com>
2024-07-10 17:06:03 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Soumil Malhotra on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
